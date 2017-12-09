2b3f0bd4144246255356a836cc6997c76d597630
Kazuya Takei <attakei@gmail.com>
2017-12-09 21:48:25 +0900

    Add gitignore command

--- act/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-import click
-
-
-@click.group()
-def act():
-    """
-    """
-    pass
--- act/cli.py
+++ act/cli.py
@@ -1,5 +1,27 @@
 # -*- coding:utf-8 -*-
-from . import act
+from urllib.request import urlopen
+import logging
+import click
+
+
+Logger = logging.getLogger(__name__)
+
+
+@click.group()
+def act():
+    pass
+
+
+@act.command()
+@click.argument('ignore_types', nargs=-1)
+def gitignore(ignore_types: tuple):
+    URL_BASE = 'https://www.gitignore.io/api'
+    ignore_types = sorted(ignore_types)
+    Logger.debug('Targets are %s', ignore_types)
+    url = '{}/{}'.format(URL_BASE, ','.join(ignore_types))
+    Logger.debug('Call URL is %s', url)
+    resp = urlopen(url)
+    click.echo(resp.read())
 
 
 def main():
