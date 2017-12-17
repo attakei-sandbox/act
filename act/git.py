5e68f88a013f3a2b337d73441423df0309e3a945
Kazuya Takei <attakei@gmail.com>
2017-12-18 02:12:54 +0900

    Split and move git related commands

---
+++ act/git.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+"""act git commands
+"""
+from urllib.request import urlopen
+import logging
+import click
+from .cli import act
+
+
+Logger = logging.getLogger(__name__)
+
+
+@act.command('git-init')
+@click.option('--path', '-p', type=click.Path(exists=True), default='.')
+@click.option('--message', '-m', type=str, default='Initial commit')
+def git_init(path, message):
+    from git import Repo
+    from git.exc import InvalidGitRepositoryError
+    try:
+        Repo(path)
+        click.echo('This folder is already git repository')
+        return
+    except InvalidGitRepositoryError:
+        pass
+    repo = Repo.init(path)
+    repo.git.add('.')
+    repo.index.commit(message)
+    click.echo(f"Initialize git repository into {path}")
+    return
+
+
+@act.command()
+@click.argument('ignore_types', nargs=-1)
+def gitignore(ignore_types: tuple):
+    """Get .gitignore output from gitignore.io
+    """
+    URL_BASE = 'https://www.gitignore.io/api'
+    ignore_types = sorted(ignore_types)
+    Logger.debug('Targets are %s', ignore_types)
+    url = '{}/{}'.format(URL_BASE, ','.join(ignore_types))
+    Logger.debug('Call URL is %s', url)
+    resp = urlopen(url)
+    click.echo(resp.read())
