f9d6c78eee3db350470c696b9a3cba0f77f389fe
Kazuya Takei <attakei@gmail.com>
2017-12-18 11:06:03 +0900

    Refactor variable

--- act/gitlab.py
+++ act/gitlab.py
@@ -9,15 +9,15 @@ Logger = logging.getLogger(__name__)
 
 
 @act.command('gitlab-ci-init')
-@click.argument('lang')
-def init_yml(lang: str):
+@click.argument('template', help='target lang and library')
+def init_yml(template: str):
     cur_dir = Path.cwd()
     cur_yml = cur_dir / '.gitlab-ci.yml'
     if cur_yml.exists():
         click.echo('.gitlab-ci.yml is already exists.')
         return
     # TODO: generate by module
-    src = ROOT / 'files' / 'gitlab' / f"gitlab-ci.{lang}.yml"
+    src = ROOT / 'files' / 'gitlab' / f"gitlab-ci.{template}.yml"
     content = src.read_text()
     cur_yml.write_text(content)
     click.echo('Generated!')
