61c6ee69f8f4fdcd7792a8d76923c2b0de665e19
Kazuya Takei <attakei@gmail.com>
2017-12-18 11:13:36 +0900

    Remove help_text: invalid args

--- act/gitlab.py
+++ act/gitlab.py
@@ -9,7 +9,7 @@ Logger = logging.getLogger(__name__)
 
 
 @act.command('gitlab-ci-init')
-@click.argument('template', help='target lang and library')
+@click.argument('template')
 def init_yml(template: str):
     cur_dir = Path.cwd()
     cur_yml = cur_dir / '.gitlab-ci.yml'
