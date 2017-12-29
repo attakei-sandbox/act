df7e26f78922578a7badca5b5696056f0c1788c1
Kazuya Takei <attakei@gmail.com>
2017-12-29 17:41:21 +0900

    Add article category

--- act/pelican.py
+++ act/pelican.py
@@ -18,7 +18,8 @@ JINJA2_ENV = Environment(
 @act.command('pelican-new')
 @click.option('--style', '-s', type=str, default='simple')
 @click.option('--title', '-t', type=str, required=True)
-def init_article(style: str, title: str):
+@click.option('--category', '-c', type=str, default='Life')
+def init_article(style: str, title: str, category: str):
     article_date = date.today()
     output_dir: Path = Path.cwd()\
         .joinpath(BASIC_PATH, str(article_date.year))
@@ -27,7 +28,7 @@ def init_article(style: str, title: str):
     article_path: Path = output_dir.joinpath('article.rst')
     template.stream(
         article_date=article_date.strftime('%Y-%m-%d'),
-        title=title)\
+        title=title, category=category)\
         .dump(str(article_path))
     click.echo(f"Generated: {article_path}")
     return
