72972ff64c54003725ceda97099b95243f84a21b
Kazuya Takei <attakei@gmail.com>
2017-12-29 17:25:23 +0900

    Refactor: rename variables

--- act/pelican.py
+++ act/pelican.py
@@ -16,15 +16,15 @@ JINJA2_ENV = Environment(
 
 
 @act.command('pelican-new')
-@click.option('--template', '-t', type=str, default='simple')
-def init_article(template: str):
+@click.option('--style', '-s', type=str, default='simple')
+def init_article(style: str):
     article_date = date.today()
     output_dir: Path = Path.cwd()\
         .joinpath(BASIC_PATH, str(article_date.year))
     output_dir.mkdir(parents=True, exist_ok=True)
-    tmpl: Template = JINJA2_ENV.get_template(f"{template}.j2")
+    template: Template = JINJA2_ENV.get_template(f"{style}.j2")
     article_path: Path = output_dir.joinpath('article.rst')
-    tmpl.stream(
+    template.stream(
         article_date=article_date.strftime('%Y-%m-%d'))\
         .dump(str(article_path))
     click.echo(f"Generated: {article_path}")
