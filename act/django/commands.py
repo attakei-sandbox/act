fcc32f795e433cceeccbedd8b34774dc56b80d24
Kazuya Takei <attakei@gmail.com>
2018-01-08 00:58:28 +0900

    Generate testing boilerplate

--- act/django/commands.py
+++ act/django/commands.py
@@ -46,3 +46,32 @@ def create_command(name: str, dest: str, comment: str):
     template: Template = JINJA2_ENV.get_template('command.py.j2')
     template.stream(**context)\
         .dump(str(dest / f"management/commands/{name}.py"))
+
+
+@django.command()
+@click.argument('name', type=click.Path(file_okay=False))
+def gen_testing(name: str):
+    """Create sources for testing
+    """
+    django_module = Path(name)
+    project_root = django_module.parent
+    project_root / 'setup.cfg'
+    context = {
+        'module_name': django_module.name,
+        'requirements': [
+            'flake8',
+            'pytest',
+            'pytest-django',
+        ]
+    }
+    (django_module / 'tests').mkdir(parents=True, exist_ok=True)
+    (django_module / 'tests/__init__.py').touch(exist_ok=True)
+    template: Template = JINJA2_ENV.get_template('testing/settings.py.j2')
+    template.stream(**context)\
+        .dump(str(django_module / 'tests/settings.py'))
+    template: Template = JINJA2_ENV.get_template('testing/setup.cfg.j2')
+    template.stream(**context)\
+        .dump(str(project_root / 'setup.cfg'))
+    template: Template = JINJA2_ENV.get_template('testing/requirements.txt.j2')
+    template.stream(**context)\
+        .dump(str(project_root / 'requirements-dev.txt'))
